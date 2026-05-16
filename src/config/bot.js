e85887df3af9d77e1d9c878a940c2f71aa92c709
rishiiiz <atulitarishi@gmail.com>
2026-05-16 16:18:24 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -88,8 +88,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699", 
-      secondary: "#2F3136", 
+      primary: "#000000", 
+      secondary: "#7D0305", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
