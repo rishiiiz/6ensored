609e6a12005b6052e253feecf6c4c6b9d571503f
rishiiiz <atulitarishi@gmail.com>
2026-05-16 16:12:33 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -27,7 +27,7 @@ export const botConfig = {
         // Text users will see (example: "Playing /help | Titan Bot").
         name: "insomnia",
         // Activity type number (0 = Playing).
-        type: 4, 
+        type: 3, 
       },
     ],
   },
